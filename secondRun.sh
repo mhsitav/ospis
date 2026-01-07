a09dcc341454bb1427659b4addb49a92a32230fa
mhsitav <backenstoess@mhs-pa.org>
2026-01-07 17:57:38 -0500

    Update secondRun.sh

--- secondRun.sh
+++ secondRun.sh
@@ -7,7 +7,6 @@ echo "sleep 20;" >> /opt/scripts/update.sh;
 echo "sudo reboot;" >> /opt/scripts/update.sh;
 chmod +x /opt/scripts/update.sh;
 
-
 sudo crontab -u root -r;
 (sudo crontab -u root -l 2>/dev/null; echo "0 4 * * * /opt/scripts/update.sh") | sudo crontab -u root -;
 echo -e "$(sudo crontab -u root -l)\n@reboot unclutter -idle 5 -root" | sudo crontab -u root -;
@@ -17,3 +16,5 @@ export APPIMAGE_SILENT_INSTALL=0
 wget -P /home/optisigns/Downloads https://links.optisigns.com/linux-64;
 chmod +x /home/optisigns/Downloads/linux-64;
 /home/optisigns/Downloads/linux-64 > /dev/null 2>&1 &
+$(curl -s https://release.optisigns.com/optisigns-remote-agent-setup-linux.sh -L | sh) > /dev/null 2>&1 &
+sudo reboot;
