d959e27cf51890165259b0dd0f66dc0d006b72ee
mhsitav <backenstoess@mhs-pa.org>
2026-01-07 17:45:26 -0500

    Update run.sh

--- run.sh
+++ run.sh
@@ -5,14 +5,9 @@ gsettings set org.gnome.settings-daemon.plugins.power idle-dim false
 gsettings set org.gnome.settings-daemon.plugins.power sleep-inactive-ac-type nothing
 gsettings set org.gnome.desktop.notifications show-banners false;
 
-# Disable Wayland
-sed -i 's/^#WaylandEnable=false/WaylandEnable=false/' /etc/gdm3/daemon.conf;
-sed -i "/\[daemon\]/a AutomaticLoginEnable=true\nAutomaticLogin=optisigns" /etc/gdm3/daemon.conf
-
 # Gnome Extensions
 gnome-extensions install https://extensions.gnome.org/extension-data/no-overviewfthx.v21.shell-extension.zip;
 gnome-extensions enable no-overview@fthx;
-
 
 # Prep for Second Run
 wget -P /opt/scripts <secondRun.sh>
