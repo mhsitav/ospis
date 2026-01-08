0dda92bc806cb53dfc3c60636988e7ba7ebb37ab
mhsitav <backenstoess@mhs-pa.org>
2026-01-08 12:44:15 -0500

    Update secondRun.sh

--- secondRun.sh
+++ secondRun.sh
@@ -11,11 +11,11 @@ crontab -u root -r;
 (crontab -u root -l 2>/dev/null; echo "0 4 * * * /opt/scripts/update.sh") | crontab -u root -;
 echo -e "$(crontab -u root -l)\n@reboot unclutter -idle 5 -root" | crontab -u root -;
 
-wget -O /home/optisigns/Desktop/RunMeFirst.sh https://raw.githubusercontent.com/mhsitav/ospis/refs/heads/main/RunMeFirst.sh
-chmod 777 /home/optisigns/Desktop/RunMeFirst.sh;
+wget -O /home/optisigns/Desktop/RunMeFirst.sh https://raw.githubusercontent.com/mhsitav/ospis/refs/heads/main/RunMe.sh
+chmod +x /home/optisigns/Desktop/RunMe.sh;
+chmod 777 /home/optisigns/Desktop/RunMe.sh;
 
-wget -O /home/optisigns/Desktop/RunMeSecond.sh https://raw.githubusercontent.com/mhsitav/ospis/refs/heads/main/RunMeSecond.sh
-chmod 777 /home/optisigns/Desktop/RunMeSecond.sh;
+(crontab -u optisigns -l 2>/dev/null; echo "@reboot bash /home/optisigns/Desktop/RunMe.sh") | crontab -u optisigns -;
 
 # Gnome Extensions
 gnome-extensions install https://extensions.gnome.org/extension-data/no-overviewfthx.v21.shell-extension.zip;
