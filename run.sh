7ed2f8903dcb448f713d68244acc1341b9f9f860
mhsitav <backenstoess@mhs-pa.org>
2026-01-08 10:43:26 -0500

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,23 +1,20 @@
 #!/bin/bash
-
+sleep 2;
 # Install additional packages
 sudo apt update && sudo apt upgrade -y;
+sleep 2;
 sudo apt install -y libfuse2 fuse gnome-shell-extension-manager unclutter;
-
+sleep 5;
 # Setup System Settings
-gsettings set org.gnome.settings-daemon.plugins.power idle-dim false
-gsettings set org.gnome.settings-daemon.plugins.power sleep-inactive-ac-type nothing
+gsettings set org.gnome.settings-daemon.plugins.power idle-dim false;
+sleep 2;
+gsettings set org.gnome.settings-daemon.plugins.power sleep-inactive-ac-type nothing;
+sleep 2;
 gsettings set org.gnome.desktop.notifications show-banners false;
 
 # Gnome Extensions
 gnome-extensions install https://extensions.gnome.org/extension-data/no-overviewfthx.v21.shell-extension.zip;
 gnome-extensions enable no-overview@fthx;
-
-#Disable Wayland
-sed -i 's/^#WaylandEnable=false/WaylandEnable=false/' /etc/gdm3/daemon.conf;
-
-#Setup Autologin
-sed -i "/\[daemon\]/a AutomaticLoginEnable=true\nAutomaticLogin=optisigns" /etc/gdm3/daemon.conf;
 
 # Prep for Second Run
 wget -P /opt/scripts https://raw.githubusercontent.com/mhsitav/ospis/refs/heads/main/secondRun.sh
