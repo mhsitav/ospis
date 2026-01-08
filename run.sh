c4146a31b5251894c77ce9455ade610e5f1dd845
mhsitav <backenstoess@mhs-pa.org>
2026-01-08 10:44:42 -0500

    Update run.sh

--- run.sh
+++ run.sh
@@ -5,16 +5,6 @@ sudo apt update && sudo apt upgrade -y;
 sleep 2;
 sudo apt install -y libfuse2 fuse gnome-shell-extension-manager unclutter;
 sleep 5;
-# Setup System Settings
-gsettings set org.gnome.settings-daemon.plugins.power idle-dim false;
-sleep 2;
-gsettings set org.gnome.settings-daemon.plugins.power sleep-inactive-ac-type nothing;
-sleep 2;
-gsettings set org.gnome.desktop.notifications show-banners false;
-
-# Gnome Extensions
-gnome-extensions install https://extensions.gnome.org/extension-data/no-overviewfthx.v21.shell-extension.zip;
-gnome-extensions enable no-overview@fthx;
 
 # Prep for Second Run
 wget -P /opt/scripts https://raw.githubusercontent.com/mhsitav/ospis/refs/heads/main/secondRun.sh
