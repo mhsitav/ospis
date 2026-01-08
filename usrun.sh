bd01d84d47e1147ffe0faee757228418a370222b
mhsitav <backenstoess@mhs-pa.org>
2026-01-08 10:44:55 -0500

    Update usrun.sh

--- usrun.sh
+++ usrun.sh
@@ -1,1 +1,12 @@
+#!/bin/bash
 
+# Setup System Settings
+gsettings set org.gnome.settings-daemon.plugins.power idle-dim false;
+sleep 2;
+gsettings set org.gnome.settings-daemon.plugins.power sleep-inactive-ac-type nothing;
+sleep 2;
+gsettings set org.gnome.desktop.notifications show-banners false;
+
+# Gnome Extensions
+gnome-extensions install https://extensions.gnome.org/extension-data/no-overviewfthx.v21.shell-extension.zip;
+gnome-extensions enable no-overview@fthx;
