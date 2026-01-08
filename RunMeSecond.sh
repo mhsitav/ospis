0cdfcce945ba0fd7aedde72c4e4dc9d51e5eed30
mhsitav <backenstoess@mhs-pa.org>
2026-01-08 12:42:05 -0500

    Update RunMeSecond.sh

--- RunMeSecond.sh
+++ RunMeSecond.sh
@@ -1,12 +1,1 @@
 #!/bin/bash
-
-gnome-extensions enable no-overview@fthx;
-
-# Download and Install Optisigns (FINAL STEP)
-export APPIMAGE_SILENT_INSTALL=0
-wget -O /home/optisigns/Downloads/linux-64 https://links.optisigns.com/linux-64;
-chmod +x /home/optisigns/Downloads/linux-64;
-/home/optisigns/Downloads/linux-64 > /dev/null 2>&1 &
-$(curl -s https://release.optisigns.com/optisigns-remote-agent-setup-linux.sh -L | sh) > /dev/null 2>&1 &
-
-rm -- "$0";
