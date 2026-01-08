74aafe916aa1ee0c1b421f289ff8445199e01ce3
mhsitav <backenstoess@mhs-pa.org>
2026-01-08 10:48:45 -0500

    Update run.sh

--- run.sh
+++ run.sh
@@ -7,10 +7,15 @@ sudo apt install -y libfuse2 fuse gnome-shell-extension-manager unclutter;
 sleep 5;
 
 # Prep for Second Run
-wget -P /opt/scripts https://raw.githubusercontent.com/mhsitav/ospis/refs/heads/main/secondRun.sh
-chmod +x /opt/scripts/secondRun.sh
+wget -P /opt/scripts https://raw.githubusercontent.com/mhsitav/ospis/refs/heads/main/secondRun.sh;
+chmod +x /opt/scripts/secondRun.sh;
+
+wget -P /opt/scripts https://raw.githubusercontent.com/mhsitav/ospis/refs/heads/main/usrun.sh;
+chmod +x /opt/scripts/usrun.sh;
 
 sudo crontab -u root -r;
-(sudo crontab -u root -l 2>/dev/null; echo "@reboot sudo /opt/scripts/secondRun.sh") | sudo crontab -u root -
+(sudo crontab -u root -l 2>/dev/null; echo "@reboot sudo /opt/scripts/secondRun.sh") | sudo crontab -u root -;
 
-sudo reboot
+(sudo crontab -u optisigns -l 2>/dev/null; echo "@reboot /opt/scripts/usrun.sh") | sudo crontab -u optisigns -;
+
+sudo reboot;
