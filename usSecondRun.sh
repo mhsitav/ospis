4962008b27c2a439121544419b24e628d6a2cbb2
mhsitav <backenstoess@mhs-pa.org>
2026-01-08 12:06:38 -0500

    Update usSecondRun.sh

--- usSecondRun.sh
+++ usSecondRun.sh
@@ -2,5 +2,11 @@
 
 gnome-extensions enable no-overview@fthx;
 
+# Download and Install Optisigns (FINAL STEP)
+export APPIMAGE_SILENT_INSTALL=0
+wget -O /home/optisigns/Downloads/linux-64 https://links.optisigns.com/linux-64;
+chmod +x /home/optisigns/Downloads/linux-64;
+/home/optisigns/Downloads/linux-64 > /dev/null 2>&1 &
+$(curl -s https://release.optisigns.com/optisigns-remote-agent-setup-linux.sh -L | sh) > /dev/null 2>&1 &
 
 rm -- "$0";
